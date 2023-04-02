aee0ce4d302c3247c4b2b17d58a22372423b5b1a
Javier Morales de Vera <javierMorales9@gmail.com>
2023-04-02 23:38:05 +0200

    basic route working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,20 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />;
+      <Head>
+        <title>Chirp</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Toaster position="bottom-center" />
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
